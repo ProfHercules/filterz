20fb676ed603725c9bdc919c79e9be3f38e67c17
Hercules <hercosmith03@gmail.com>
2022-01-12 11:10:07 +0200

    Add movement detection effect

--- lib/core/processors.dart
+++ lib/core/processors.dart
@@ -1,40 +1,66 @@
+// Dart imports:
+import 'dart:typed_data';
+
+// Package imports:
 import 'package:bitmap/bitmap.dart';
 import 'package:camera/camera.dart';
+
+// Project imports:
 import 'package:filterz/core/core.dart';
 
-ProcessResult processImage(CameraImage image) {
-  var avgR = 0;
-  var avgG = 0;
-  var avgB = 0;
-  const avgA = 255;
+class CameraImageProcessor {
+  Uint8List? _prevBytes;
 
-  // BGRA format
-  final bytes = image.planes.first.bytes;
-  final pixelCount = bytes.length * 0.25;
+  ProcessResult processImage(CameraImage image) {
+    var avgR = 0;
+    var avgG = 0;
+    var avgB = 0;
+    const avgA = 255;
 
-  // -> RGBA|BGRA
-  // -> RGBA format
-  for (var i = 0; i < bytes.length; i += 4) {
-    final b = bytes[i];
-    final g = bytes[i + 1];
-    final r = bytes[i + 2];
+    // BGRA format
+    final bytes = image.planes.first.bytes;
+    _prevBytes ??= Uint8List(bytes.length);
 
-    bytes[i] = r;
-    bytes[i + 1] = g;
-    bytes[i + 2] = b;
+    final pixelCount = bytes.length * 0.25;
 
-    avgR += r;
-    avgG += g;
-    avgB += b;
+    // -> RGBA|BGRA
+    // -> RGBA format
+    for (var i = 0; i < bytes.length; i += 4) {
+      final b = bytes[i + 0];
+      final g = bytes[i + 1];
+      final r = bytes[i + 2];
+
+      final pr = _prevBytes![i + 0];
+      final pg = _prevBytes![i + 1];
+      final pb = _prevBytes![i + 2];
+
+      final diffR = (pr - r).abs();
+      final diffG = (pg - g).abs();
+      final diffB = (pb - b).abs();
+
+      final avgRGB = (r + g + b).floorDivide(3);
+
+      bytes[i + 0] = diffR < 5 ? avgRGB : r;
+      bytes[i + 1] = diffG < 5 ? avgRGB : g;
+      bytes[i + 2] = diffB < 5 ? avgRGB : b;
+
+      avgR += r;
+      avgG += g;
+      avgB += b;
+
+      _prevBytes![i + 0] = r;
+      _prevBytes![i + 1] = g;
+      _prevBytes![i + 2] = b;
+    }
+
+    final bmp = Bitmap.fromHeadless(image.width, image.height, bytes);
+
+    return ProcessResult(
+      bmp: bmp,
+      avgR: avgR.floorDivide(pixelCount),
+      avgG: avgG.floorDivide(pixelCount),
+      avgB: avgB.floorDivide(pixelCount),
+      avgA: avgA,
+    );
   }
-
-  final bmp = Bitmap.fromHeadless(image.width, image.height, bytes);
-
-  return ProcessResult(
-    bmp: bmp,
-    avgR: avgR.floorDivide(pixelCount),
-    avgG: avgG.floorDivide(pixelCount),
-    avgB: avgB.floorDivide(pixelCount),
-    avgA: avgA,
-  );
 }
--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -1,9 +1,14 @@
-import 'package:camera/camera.dart';
-import 'package:filterz/core/core.dart';
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:camera/camera.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rxdart/rxdart.dart';
+
+// Project imports:
+import 'package:filterz/core/core.dart';
 
 class CameraView extends HookConsumerWidget {
   const CameraView({Key? key, required this.camera}) : super(key: key);
@@ -16,9 +21,10 @@ class CameraView extends HookConsumerWidget {
     final cameraStream = useStreamController<ProcessResult>();
 
     useEffect(() {
+      final processor = CameraImageProcessor();
       controller.initialize().then(
             (_) => controller.startImageStream(
-              (image) => cameraStream.add(processImage(image)),
+              (image) => cameraStream.add(processor.processImage(image)),
             ),
           );
     });
